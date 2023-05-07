87ff8b7ea77018749845d85e6a4a08b9c23ed4da
Peter Bushnell <bushsolo@gmail.com>
2023-05-07 10:15:35 +0100

    Demo map loading error

---
+++ assets/simple-isometric.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="simple-isometric" tilewidth="64" tileheight="64" tilecount="54" columns="6">
+ <image source="simple-isometric.png" width="384" height="576"/>
+</tileset>
